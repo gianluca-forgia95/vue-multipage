b2e9d7393e4f3910653abd2cd9d008c736ace689
Gianluca Forgia <gianluca.forgia@gmail.com>
2021-07-10 17:31:28 +0200

    404 page + show

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
 import Jobs from '../views/jobs/Jobs.vue'
+import JobDetails from '../views/jobs/JobDetails.vue'
+import ErrorPage from '../views/ErrorPage.vue'
 
 const routes = [
   {
@@ -18,6 +20,23 @@ const routes = [
     path: '/jobs',
     name: 'Jobs',
    component: Jobs
+  },
+  {
+    path: '/jobs/:id',
+    name: 'JobDetails',
+    component: JobDetails,
+    props: true
+  },
+  //redirect
+  {
+    path: '/all-jobs',
+    redirect: '/jobs'
+  },
+  //catch404
+  {
+    path: '/:catchAll(.*)',
+    name: 'ErrorPage',
+    component: ErrorPage,
   }
 ]
 
--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -1,7 +1,9 @@
 <template>
-  <h1>jobs</h1>
-   <div v-for="job in jobs" :key="job.id">
+  <h1>Jobs</h1>
+   <div class="job-container" v-for="job in jobs" :key="job.id">
+     <router-link :to="{ name: 'JobDetails', params: { id: job.id } }">
       <h2>{{ job.title }}</h2>
+    </router-link>
 
    </div>
 
@@ -43,7 +45,30 @@ data() {
 </script>
 
 <style scoped>
+h1 {
+  text-transform: uppercase;
+  font-size: 26px;
+  color: crimson;
+}
 
+.job-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 400px;
+  margin: auto;
+}
+
+.job-container a {
+  text-decoration: none;
+  color: #444;
+  padding: 20px;
+}
+ 
+.job-container a :hover {
+  background: #ddd;
+  border-radius: 11px;
+}
 </style>
 
 
