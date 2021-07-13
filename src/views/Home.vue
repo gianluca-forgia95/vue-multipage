d42a5d0e138fdf315d888b2b48cf30efd4a4c3ee
Gianluca Forgia <gianluca.forgia@gmail.com>
2021-07-13 11:02:37 +0200

    netflix api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,13 +3,102 @@
     <h1>Homepage</h1>
     <p>This is the homepage of the website</p>
   </div>
+
+   <div class="search-films">
+        <input class="input-search" type="text" placeholder="search movie or tv-series..."  v-model="search">
+        <button class="btn" type="button" name="button" @click="searchFilm">Search</button>
+   </div>
+
+     <!-- Starter Main -->
+     <div class="start" v-if="movies.length == 0 ">
+       <h1 class="title-base">Benvenuto su Boolflix, inizia cercando Film o SerieTv</h1>
+       <img  src="img/netflix-background.jpg" alt="">
+     </div>
+    <!-- /Starter Main -->
+    <h2 class="title-section" v-if="movies.length > 0">Movies</h2>
+      <div class="container">
+        <!-- Movie Card -->
+        <div class="card" v-for="movie in movies" :key='movie.id' >
+          <div class="card-inner">
+            <!-- Img Cover -->
+           <img :src="'https://image.tmdb.org/t/p/w300' + movie.poster_path" alt="Movie img" class="cover">
+            <!-- /Img Cover -->
+            <!-- Info Movie -->
+           <div class="info">
+             <h3 class="title">Titolo: {{ movie.title }}</h3>
+             <h4>Titolo Originale: {{ movie.original_title }}</h4>
+            <!-- Movie Original Language -->
+            <div class="language">
+              <span>Lingua Originale
+                <img :src="'flags/' + movie.original_language + '.png'">
+              </span>
+             </div>
+            <!-- /Movie Original Language -->
+            <!-- Movie Rating -->
+           <div class="rating">
+            <span>Rated:
+             <!-- <i v-for="index in movie.vote_average" class="star fas fa-star"></i>
+             <i v-for="index in (5 - movie.vote_average)" class="star far fa-star"></i> -->
+            </span>
+           </div>
+           <!-- /Movie Rating -->
+           <!-- Realease Date -->
+           <div class="release-date">
+             <span>Release: {{ movie.release_date }}</span>
+           </div>
+           <!-- /Realease Date -->
+           <!-- Overview -->
+           <h4 class="title-prev" v-if="movie.overview.length > 0">Overview</h4>
+           <div class="overview" >
+            <p>{{ movie.overview }}</p>
+           </div>
+           <!-- /Overview -->
+          </div>
+       <!-- /Info Movie -->
+         </div>
+       </div>
+      <!-- /Movie Card -->
+      </div>
 </template>
 
 <script>
 
 
+
 export default {
   name: 'Home',
-  components: {}
+  components: {},
+  data() {
+    return {
+      movies: [],
+      search: "",
+      apiKey: 'febf9a4fc7b46ba8b5e4681cf81209ce',
+      urlSearch: 'https://api.themoviedb.org/3/search/',
+    }
+  },
+  methods: {
+    searchFilm() {
+         axios.get( this.urlSearch + 'movie' , {
+         params: {
+           api_key: this.apiKey,
+           query: this.search,
+           language: "it-IT"
+         }
+       })
+       .then( (response) => {
+         this.movies = response.data.results;
+         console.log(this.movies);
+         this.search = "";
+         for ( var i = 0; i < this.movies.length; i++ ) {
+           // Voto 1/5
+            this.movies[i].vote_average = Math.ceil(this.movies[i].vote_average / 2);
+           
+         }
+
+       });
+
+    }
+   
+  }
 }
 </script>
